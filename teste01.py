3bb2ba45668804f6aa82e596b7a07b87ee484a76
unknown <vivianne.chaves17@gmail.com>
2020-02-12 15:16:34 -0300

    nada

--- teste01.py
+++ teste01.py
@@ -1,3 +1,26 @@
-print("ola mundo")
+class Complex:
+    def __init__(self):
+        self.r = 0.0
+        self.i = 0.0
+    
+    def set_i(self,i):
+        self.i = i
+    
+    def get_i(self):
+        return self.i
+    
+    def set_r(self,r):
+        self.r = r
+    
+    def get_r(self):     
+        return self.r
+    
+
+x = Complex()
+x.set_r(3.0)
+x.set_i(-2.0)
+
+print(x.r, x.i)
+
 
     
