8a5db5ad4b29794ea94a3efc2d6e118560e2b7f0
VivianneChaves <vivianne.chaves17@gmail.com>
2020-08-07 19:33:24 -0300

    update

--- Projeto01/cliente.py
+++ Projeto01/cliente.py
@@ -1,20 +1,17 @@
-from tipocliente import TipoCliente
+from tipocliente  import TipoCliente
+
 class Cliente():
-    
-    def __init__(self,ide,nome,codigo,cnpjcpf,tipo): 
+    def __init__(self, id, nome, codigo, cnpjcpf, tipo):
         self._id = id
-        self._nome = nome 
-        self._codigo = codigo 
+        self._nome = nome
+        self._codigo = codigo
         self._cnpjcpf = cnpjcpf
         self._tipo = tipo
-    
+        
     def str(self):
-        string = ("ID: {0}","Nome: {1}","Codigo: {2}","Cnpj/CPF: {3}","Tipo: {4}").format(self._id,self._nome,self._codigo,self._cnpjcpf,self._tipo)
+        string="\nID= {4}\nCodigo= {3}\nNome= {2}\nCNPJ/CPF= {1}\nTipo= {0}\n".format(self._tipo, self._cnpjcpf, self._codigo, self._nome, self._id)
         return string
     
-    if __name__ == ' __main__':
-        cliente = Cliente(1,"Ana", 102 ,147 ,1)
-        
-       
-   
-    
+if __name__ == '__main__':
+    cliente=Cliente(1, "Jose Maria", 100, '200.100.345-34', TipoCliente.PESSOA_FISICA)
+    print(cliente.str())
--- Projeto01/produto.py
+++ Projeto01/produto.py
@@ -1,12 +1,32 @@
+"""
+    Módulo produto
+    Classe Produto
+    Atributos :
+        id            - informado
+        codigo        - informado
+        descricao     - informado
+        valorUnitario - informado. 
+"""
 class Produto():
-    def __init__(self, id, codigo, desc, valor):
-        self.id = id
-        self.codigo = codigo
-        self.desc = desc
-        self.valor = valor
+
+    def __init__(self, id, codigo, descricao, valorUnitario):
+        self._id = id
+        self._codigo=codigo
+        self._descricao=descricao
+        self._valorUnitario=valorUnitario
+        
+    def getDescricao(self):
+        return self._descricao
+    
+    def getValorUnitario(self): 
+        return self._valorUnitario
         
     def str(self):
-        string = "Id = {3} Código = {2} Descrição = {1} Valor = {0}".format (self.valor,self.desc,self.codigo,self.id)
-        return string    
+        string="\nId={3} Codigo={2} Descricao={1} Valor Unitario={0}".format(self._valorUnitario, self._descricao, self._codigo, self._id)
+        return string
     
+   
     
+if __name__ == '__main__':
+    produto=Produto(1,100,'Arroz', 5.5)        
+    print(produto.str())
